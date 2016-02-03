c2ee417373342502f4ed2366800d83b6c34c1ae7
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-02 19:16:19 -0500

    recipe files

---
+++ kitchingroup-58.el
@@ -0,0 +1,35 @@
+;;; kitchingroup-58.el --- CaPPA preprint for doi:10.1021/acs.jpcc.5b05338
+
+;; Copyright (C) 2016 John Kitchin
+
+;; Version: 0.0.1
+;; Author: Matthew Curnan
+;;   John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+;; DOI: 10.1021/acs.jpcc.5b05338
+;; Journal: The Journal of Physical Chemistry C
+;; Bibtex: @article{curnan-2015-inves-energ,
+;;   author =       {Matthew Curnan and John R. Kitchin},
+;;   title =        {Investigating the Energetic Ordering of Stable and Metastable
+;;                   TiO$_2$ Polymorphs Using DFT+U and Hybrid Functionals},
+;;   journal =      {The Journal of Physical Chemistry C},
+;;   volume =	 {119},
+;;   year =         2015,
+;;   doi =          {10.1021/acs.jpcc.5b05338},
+;;   url =          { http://dx.doi.org/10.1021/acs.jpcc.5b05338 },
+;;   eprint =       { http://dx.doi.org/10.1021/acs.jpcc.5b05338 },
+;; }
+
+;;; Commentary:
+
+;; Investigating the Energetic Ordering of Stable and Metastable
+;;                   TiO$_2$ Polymorphs Using DFT+U and Hybrid Functionals
+
+;;; Code:
+(require 'cappa)
+
+(cappa-register 'kitchingroup-58)
+
+
+(provide 'kitchingroup-58)
+;;; kitchingroup-58.el ends here
